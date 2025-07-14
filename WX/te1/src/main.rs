258d9e2d37131eebe0a908d861579afa38bf4d4d
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-07-14 06:41:49 +0000

    🫦

--- WX/te1/src/main.rs
+++ WX/te1/src/main.rs
@@ -5,11 +5,13 @@
 // Imports 
  
  mod utilz;
- use utilz::{clear_console,header};
+ use utilz::{clear_console,header,pswg};
 
 // Main Logic
 
 fn main() {
+    pswg("Whootey".to_string());
+    clear_console();
     println!("Hello, world!");
     header("Wilka");
 }
