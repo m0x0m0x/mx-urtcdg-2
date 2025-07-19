c4d3db655dd2730d168595773bdb637d19713954
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-07-19 10:29:56 +0000

    🫦

--- WX/w1/src/sec2/s1.rs
+++ WX/w1/src/sec2/s1.rs
@@ -3,13 +3,15 @@
 // s1.rs = Section 1 of the work
 //-----------------------------------------
 
+//
+
 // --- Imports ---
 use crate::utilz::{clear_console, pswg};
 
 // --- main ---
 pub fn s1_main() {
     clear_console();
-    s1();
+    s2();
 }
 
 // --- Sub ---
@@ -19,4 +21,10 @@ fn s1() {
     // Function header
     let t1 = "src/sec2/s1 main";
     pswg(t1.to_string())
+}
+
+// 7 - Representing data with structs
+fn s2() {
+    let t1 = "7: Representing data with structs";
+    pswg(t1.to_string());
 }
