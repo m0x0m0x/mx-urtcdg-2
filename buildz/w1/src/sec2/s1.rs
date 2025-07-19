2592d092f1aadb98d305e668b66c2d649fbbc7ad
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-07-19 12:46:16 +0000

    👙

---
+++ buildz/w1/src/sec2/s1.rs
@@ -0,0 +1,60 @@
+//-----------------------------------------
+// Will write the chapters that are being worled on
+// s1.rs = Section 1 of the work
+//-----------------------------------------
+
+// --- attributes ---
+#![allow(dead_code)]
+
+// --- Imports ---
+use crate::utilz::{clear_console, pswg};
+use yansi::Paint;
+
+// --- main ---
+pub fn s1_main() {
+    clear_console();
+    s2();
+}
+
+// --- Sub ---
+
+// Test s1 sub function
+fn s1() {
+    // Function header
+    let t1 = "src/sec2/s1 main";
+    pswg(t1.to_string())
+}
+
+// 7 - Representing data with structs
+
+// Deck struct for s2
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+// #[allow(unused_variables)]
+fn s2() {
+    let t1 = "7: Representing data with structs";
+    pswg(t1.to_string());
+
+    // List of suits
+    let suits = ["Hearts", "Diamonds"];
+    let values = ["Ace", "Jack"];
+
+    let mut cards = vec![];
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value.blue(), suit.green());
+            println!("{}", card);
+
+            // Add card to deck
+            cards.push(card);
+        }
+    }
+
+    // Instancing the Deck Struct
+    let deck = Deck { cards: vec![] };
+    println!("{}: {:#?}", "Deck".blue(), deck.green())
+}
