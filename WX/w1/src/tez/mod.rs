a3b1e7508fde775f5a56ab73fd2ab4022dd19e57
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-07-19 09:52:42 +0000

    🫦

--- WX/w1/src/tez/mod.rs
+++ WX/w1/src/tez/mod.rs
@@ -3,8 +3,14 @@
 // ----------------------------------------------
 
 // --- Imports ---
-use crate::utilz::{}
+use crate::utilz::{clear_console, header, pswg};
 
 // -- Main function ---
+
+pub fn main_tez() {
+    clear_console();
+    t1 = "Tezt Caret Print";
+    pswg(t1.to_string());
+}
 
 // -- Subs
