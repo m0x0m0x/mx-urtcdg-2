4def1ce68e25b73eedbd6ad8b1059f20d1436b25
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-07-20 13:00:59 +0000

    🫦

--- WX/w1/src/tez/mod.rs
+++ WX/w1/src/tez/mod.rs
@@ -4,6 +4,7 @@
 
 // --- Imports ---
 use crate::utilz::{clear_console, header, pswg};
+use boxy_cli::prelude::*;
 
 // -- Main function ---
 
@@ -17,5 +18,20 @@ pub fn main_tez() {
 // Testing out boxy-clis
 fn boxy1() {
     let t1 = "Boxy Test 1";
-    pswg(t1.to_string())
+    pswg(t1.to_string());
+
+    let mut my_box = Boxy::builder()
+        .box_type(BoxType::Double) // Set border style
+        .color("#00ffff") // Set border color
+        .padding(
+            BoxPad::uniform(1),            // External padding
+            BoxPad::from_tldr(2, 2, 1, 1), // Internal padding
+        )
+        .align(BoxAlign::Center) // Center the box in the terminal
+        .add_segment("Hello, Boxy!", "#ffffff", BoxAlign::Center)
+        .add_line("This is a new line.", "#32CD32")
+        .add_segment("Another section", "#663399", BoxAlign::Left)
+        .width(50) // Set fixed width
+        .build();
+    my_box.display();
 }
