de5b0466b8f51e6710b193f28181f5c88131f698
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-07-20 13:11:09 +0000

    🫦

--- WX/w1/src/tez/box1.rs
+++ WX/w1/src/tez/box1.rs
@@ -5,7 +5,6 @@
 // -- Attributes ---
 #![allow(unused_imports)]
 #![allow(unused_variables)]
-#![allow(unused_functions)]
 
 // --- Imports ---
 use crate::utilz::{clear_console, header, pswg};
@@ -47,4 +46,20 @@ fn boxy2() {
     pswg(t2.to_string());
 
     let vec1 = vec!["Item 1", "Item 2", "Item 3"];
+
+    let mut my_box = Boxy::builder()
+        .box_type(BoxType::Double) // Set border style
+        .color("#00ffff") // Set border color
+        .padding(
+            BoxPad::uniform(1),            // External padding
+            BoxPad::from_tldr(2, 2, 1, 1), // Internal padding
+        )
+        .align(BoxAlign::Left) // Center the box in the terminal
+        .add_segment("Variables Test", "#ffffff", BoxAlign::Center)
+        .add_line("This is a new line.", "#32CD32")
+        .add_line("This is a new line.", "#32CD32")
+        .add_segment("Another section", "#663399", BoxAlign::Left)
+        .width(40) // Set fixed width
+        .build();
+    my_box.display();
 }
