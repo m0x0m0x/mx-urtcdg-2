5b407124e749ff0400315da0509f7a406cfbf34f
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-07-23 11:28:04 +0000

    🫦

--- WX/w1/jstest/sec3.js
+++ WX/w1/jstest/sec3.js
@@ -1,7 +1,26 @@
 #!/usr/bin/env bun
 
 /* 
-This is for testing shell js scripts
+Section3 Chapter 20 Work
 */
 
 // --- Imports ---
+import boxen from "boxen"
+import chalk from "chalk"
+
+// --- Main Function ---
+
+function title(textz, titlez) {
+  console.log(
+    boxen(chalk.yellow(textz), {
+      padding: 0.7,
+      textAlign: "center",
+      title: titlez,
+      titleAlignment: "center",
+      borderColor: "green",
+      borderStyle: "doubleSingle",
+    })
+  )
+}
+
+// -- Sub Function called in Main ---
