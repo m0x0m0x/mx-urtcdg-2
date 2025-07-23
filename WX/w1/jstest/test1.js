8a03c90319434e9ad6103136216691b539bd422f
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-07-23 11:10:29 +0000

    🫦

--- WX/w1/jstest/test1.js
+++ WX/w1/jstest/test1.js
@@ -6,10 +6,15 @@ This is for testing shell js scripts
 
 import chalk from "chalk"
 
+// --- Main Function Call ---
 
-// Printing text function 
+func
 
-const function writeText() {
-    console.log(chalk.blue("Hello world!"))
+// -- Sub Function Calls
+// Printing text function
+function writeText(params) {
+  console.log(chalk.blue("Hello world!"))
 }
 
+// --- execution zone ---
+writeText()
